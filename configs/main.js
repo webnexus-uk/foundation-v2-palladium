9a689857a347a64165b3bd1d9b57b178e9b1bfbd
Coding Nexus <info@webnexus.co.uk>
2024-04-12 09:56:23 +0100

    Update main.js

--- configs/main.js
+++ configs/main.js
@@ -9,24 +9,24 @@
 
 // Miscellaneous Configuration
 const config = {};
-config.language = 'english';
-config.identifier = '';
+config.language = "english";
+config.identifier = "";
 
 // Logger Configuration
 config.logger = {};
 config.logger.logColors = true;
-config.logger.logLevel = 'log';
+config.logger.logLevel = "debug";
 
 // Clustering Configuration
 config.clustering = {};
 config.clustering.enabled = true;
-config.clustering.forks = 'auto';
+config.clustering.forks = "auto";
 
 // TLS Configuration
 config.tls = {};
-config.tls.ca = '';
-config.tls.key = '';
-config.tls.cert = '';
+config.tls.ca = "";
+config.tls.key = "";
+config.tls.cert = "";
 
 // Export Configuration
 module.exports = config;
